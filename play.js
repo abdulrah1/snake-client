39e8bcee002aa496f430ce7e9b35844c2fae2a80
Abdul <ffred7760@gmail.com>
2022-11-24 16:47:47 -0500

    added client.js that requires connect function from play.js; WORKS

--- play.js
+++ play.js
@@ -1,29 +1,3 @@
-// This will serve as your main file through which you will launch the game client.
-// This is the client's side
-
-
 const net = require("net");
+const connect = require('./client');
 
-// establishes a connection with the game server
-const connect = function () {
-
-  
-  const conn = net.createConnection({
-    host: '172.31.117.246',
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  //handle incoming data from the server and console.log it for the player.
-  conn.on("data", (data) => {
-  console.log('Server says: ', data)
-});
-
-  ////The conn object that Node returned to you represents the connection that you have with the server.
-  return conn;
-};
-
-console.log("Connecting ...");
-connect();
